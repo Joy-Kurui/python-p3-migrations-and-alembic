dd5de10a795871d98952900f62a49e20cd37b64a
joy Kurui <kurui.chepngetich@gmail.com>
2024-01-10 23:46:50 +0300

    Completed Assignment

---
+++ app/migrations/versions/075107a65670_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 075107a65670
+Revises: 
+Create Date: 2024-01-10 23:19:50.399938
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '075107a65670'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
